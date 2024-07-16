63a03f810d0d9a5567635c96a92d19eef7078f13
Anwesha0425 <anwesharanigouda@gmail.com>
2024-07-16 15:28:32 +0530

    add changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import Sidebar from '@/components/Sidebar';
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+    const loggedIn ={firstName: 'Anwesha' , lastName: 'Gouda'};
     return (
-      <main>
-        SIDEBAR 
+      <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
       </main>
     );
